cc6d3810b33f6e34c5a76ea08429dd17dff53c58
NoodleOfDeath <git@noodleofdeath.com>
2019-03-09 14:16:19 -0500

    Initial commit

---
+++ Example/SwiftyUTType/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftyUTType
+//
+//  Created by NoodleOfDeath on 02/12/2019.
+//  Copyright (c) 2019 NoodleOfDeath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

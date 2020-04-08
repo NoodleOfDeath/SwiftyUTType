4678e3f4cbd8a6d4baa58e5cdd6058be7c6b99c1
NoodleOfDeath <thom@noodleofdeath.com>
2020-04-08 17:00:11 -0400

    framework support for OSX

--- SwiftyUTType/Classes/com/noodleofdeath/swiftyuttype/UTType.swift
+++ SwiftyUTType/Classes/com/noodleofdeath/swiftyuttype/UTType.swift
@@ -22,7 +22,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#if os(iOS)
 import MobileCoreServices
+#else
+import CoreServices
+#endif
 
 // MARK: - Additional Global `CFString` kUTType Constants
 
